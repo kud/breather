4622f9c037aa7a5ce440330750f22c6b4b15504d
Erwann Mest <erwann.mest@gmail.com>
2013-12-09 13:53:12 +0100

    Progress bar added.

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var notifier = require( 'node-notifier' )
+  , ProgressBar = require('progress')
 
-var time = 60 * 60 * 1000
+console.log('\n=> Working time! 👷\n')
 
-setTimeout( function() {
-  notifier.notify({
-    title: 'Break! Break! Break!',
-    message: 'Time to take a break, mate. Take a cup of tea and relax. 🍵'
-  })
-}, time )
+var bar = new ProgressBar('Time remaining before break: [:bar] :percent', {
+    total: 60
+  , width: 20
+  , complete: '='
+  , incomplete: ' '
+})
+
+bar.tick(0)
+
+var timer = setInterval(function(){
+
+  if ( bar.complete ) {
+    notifier.notify({
+      title: 'Break! Break! Break!',
+      message: 'Time to take a break, mate. Take a cup of tea and relax. 🍵'
+    })
+    clearInterval( timer )
+  }
+
+  bar.tick()
+
+}, 60 * 1000)
