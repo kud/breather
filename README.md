defaa9e61c0d77999dde9a810548585b77eb5b89
Erwann Mest <erwann.mest@gmail.com>
2013-12-09 11:17:43 +0100

    Add a readme.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Break! Break! Break!
+
+A break taker, like Kitkat but better.
+
+## Install
+
+```
+$ make install
+```
+
+## Usage
+
+_(by default, the notification will come after 1 hour)_
+
+```
+$ make start
+```
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var time = 60 * 60 * 1000
 
 setTimeout( function() {
   notifier.notify({
-    title: 'Break break break!',
+    title: 'Break! Break! Break!',
     message: 'Time to take a break, mate. Take a cup of tea and relax. 🍵'
   })
 }, time )
