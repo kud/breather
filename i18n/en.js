ddf9712362c6ff9bc169ade6c31e514c37a4f5c3
Matthias Le Brun <mlbli@me.com>
2013-12-10 12:03:00 +0100

    Added weather support

---
+++ i18n/en.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title : "Breather",
+  workMessage : "but it's time to work for the moment. 👷",
+  breakNotification : "Time to take a breath. Take a cup of tea and relax. 😌",
+  progressBar : "Time remaining before break: [:bar] :percent",
+  weather : {
+    temperature : {
+      "-50" : "Just look at the window, it's fucking cold out there. ⛄️",
+      "0" : "You really don't want to go outside right now. ❄️",
+      "10" : "Get a coat if you want to take a breath outside. ⛅️",
+      "30" : "Go outside, the weather is perfect! ☀️",
+      "Infinity" : "Go outside and you'll burn. 🔥"
+    }
+  }
+}
