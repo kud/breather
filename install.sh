8e36c7f8d1b96ac580f72c1a1fde1f01e58d5521
Erwann Mest <m@kud.io>
2014-05-10 17:51:34 +0200

    Update.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "\n> Installing ruby dependencies..."
+echo "\n❯ Installing ruby dependencies..."
 gem install terminal-notifier
-echo "\n> Copying configuration..."
-cp config/.breather ~/.breather
-echo "\n> Done!"
+echo "\n❯ Copying configuration..."
+cp -n config/.breather ~/.breather
+echo "\n❯ Done!"
